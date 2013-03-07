b2804155e4bf339c84bfd8707b61b8c96a312159
ZeusAllMighty11 <youraveragecamper@gmail.com>
2013-03-06 21:43:13 -0500

    blah blah blah stuff

--- Ban Yo' Self/src/me/zeus/BanYoSelf/BanYoSelf.java
+++ Ban Yo' Self/src/me/zeus/BanYoSelf/BanYoSelf.java
@@ -1,13 +1,17 @@
 package me.zeus.BanYoSelf;
 
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class BanYoSelf {
+
+public class BanYoSelf extends JavaPlugin {
     
     public BanYoSelf(){
         
     }
     
-    
-    
+    public void onEnable(){
+        getCommand("banme").setExecutor(new BanMeCommand(this));
+        getCommand("kickme").setExecutor(new KickMeCommand(this));
+    }
     
 }
